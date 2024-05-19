98fda25dd9342dbccc26eb2860d90e089745751a
tasif498 <tasif498@gmail.com>
2024-05-20 02:26:37 +0500

    Updated Routes

--- src/App.js
+++ src/App.js
@@ -5,15 +5,15 @@ import Dashboard from './components/Dashboard';
 import DashHistory from './components/DashHistory';
 
 function App() {
-  const basename = process.env.NODE_ENV === 'production' ? '/SpeechInsight-Frontend' : '/';
   return (
-    <Router basename={basename}>
+    <Router basename="/">
       <div>
         <Routes>
           <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/history" element={<DashHistory />} />
+
         </Routes>
       </div>
     </Router>
